ffbd7cdd1406c578b3e913b8cb01b0953dc02264
reddsaut <osauter@macalester.edu>
2025-03-14 09:43:18 -0500

    added a widget that doesn't do anything

---
+++ MiniPlant/MiniPlantBundle.swift
@@ -0,0 +1,17 @@
+//
+//  MiniPlantBundle.swift
+//  MiniPlant
+//
+//  Created by redding sauter on 3/14/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MiniPlantBundle: WidgetBundle {
+    var body: some Widget {
+        MiniPlant()
+        MiniPlantControl()
+    }
+}
