39d527974ed335545b865e9a521a81b53521e617
reddsaut <osauter@macalester.edu>
2025-04-11 14:11:32 -0500

    started enum code for unit

--- Plant/HydrationData.swift
+++ Plant/HydrationData.swift
@@ -11,10 +11,34 @@ class HydrationData: ObservableObject {
     
     @AppStorage("waterIntake") var waterIntake: Int = 0
     @AppStorage("dailyGoal") var dailyGoal: Int = 8  // daily goal (in glasses)
-    @AppStorage("unit") var unit: String = "oz"  // Default unit
+    @AppStorage("unit") var unit: Unit = .ounces  // Default unit
     @AppStorage("glassSize") var glassSize: Double = 8.0
 
     let litersPerOz = 0.0295735
+    
+    // hd.unit.format(amountInMilliliters: hd.dailyGoal)
+    
+    enum Unit: String {
+        case ounces = "oz"
+        case liters = "L"
+        
+        func format(amountInMilliliters: Double) -> String {
+            switch self {
+                case .ounces:
+                    return "\(amountInMilliliters / 1000.0) L"
+                case .liters:
+                    return "\(amountInMilliliters / 1234.0) oz"
+            }
+        }
+        func format(amountInMilliliters: Double) -> Double {
+            switch self {
+                case .ounces:
+                    return amountInMilliliters / 1000.0
+                case .liters:
+                    return amountInMilliliters / 1234.0
+            }
+        }
+    }
 
     func logGlassOfWater() {
         if waterIntake < dailyGoal {
@@ -28,7 +52,7 @@ class HydrationData: ObservableObject {
         dailyGoal = newGoal
     }
     
-    func switchUnit(to newUnit: String) {
+    func switchUnit(to newUnit: Unit) {
         unit = newUnit
     }
     
@@ -38,12 +62,15 @@ class HydrationData: ObservableObject {
 
     func getTotalIntake() -> Double {
         let totalOz = Double(waterIntake) * glassSize
-        return unit == "oz" ? totalOz : totalOz * litersPerOz
+        // return unit == "oz" ? totalOz : totalOz * litersPerOz
+        
+        return unit.format(amountInMilliliters: totalOz)
     }
 
     func getDailyGoal() -> Double {
         let goalOz = Double(dailyGoal) * glassSize
-        return unit == "oz" ? goalOz : goalOz * litersPerOz
+        // return unit == "oz" ? goalOz : goalOz * litersPerOz
+        return unit.format(amountInMilliliters: goalOz)
     }
 
     func resetDailyIntake() {
