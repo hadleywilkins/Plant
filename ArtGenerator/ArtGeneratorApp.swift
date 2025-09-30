9f5d8585c19e8547090a4ec58e74ef14b91f214d
RishikaKundu <144388152+RishikaKundu@users.noreply.github.com>
2025-09-29 22:01:01 -0500

    Update ArtGeneratorApp.swift

--- ArtGenerator/ArtGeneratorApp.swift
+++ ArtGenerator/ArtGeneratorApp.swift
@@ -5,6 +5,8 @@
 //  Created by Rishika Kundu on 9/29/25.
 //
 
+
+
 import SwiftUI
 import SpriteKit
 
@@ -18,8 +20,24 @@ import SpriteKit
 struct ArtGeneratorApp: App {
     var body: some Scene {
         WindowGroup {
-            SpriteView(scene: plantScene, options: [.allowsTransparency])
-                .frame(width: 400, height: 400)
+            VStack {
+                SpriteView(scene: plantScene, options: [.allowsTransparency])
+                    .frame(width: 400, height: 400)
+                
+                
+                Button("Generate snapshots") {
+                    // https://stackoverflow.com/a/61720423/239816
+                    
+                    plantScene.setPlantHealth(100)
+                        
+                    let result = (plantScene.view?.texture(from: plantScene))!
+                    let image = NSImage(cgImage: result.cgImage(), size: result.size())
+                    print("------>", image)
+                    
+                    
+                    // get image, save to /tmp/plant.png
+                }
+            }
         }
     }
 }
