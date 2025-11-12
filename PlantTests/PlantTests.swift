32c78f8c3fa5d56f2928ee4ad11d4f3d04128069
NurainJ <109047209+NurainJ@users.noreply.github.com>
2025-11-12 11:38:20 -0600

    Tested and fixed default values

--- PlantTests/PlantTests.swift
+++ PlantTests/PlantTests.swift
@@ -34,14 +34,33 @@ class PlantTests {
         #expect(result2 == 3.381405650328842)
     }
 
-    @Test func testOuncesRounding() async throws {
+    @Test func testGetTotalIntakeFormatted() async throws {
         
         let hd = HydrationData()
-//        hd.resetDailyIntake()
-//        let testCase = HydrationData.Unit.ounces
-        HydrationData().waterIntake = 0
-        let result = hd.getTotalIntakeFormatted()
-        #expect(result == "0.0 oz")
+        
+        let dailyGoal_oz = 64.0
+        let glassSize_oz = 8.0
+        
+        hd.waterIntake = 0
+        hd.dailyGoal = 1892.71 //64 oz in mL
+        hd.unit = HydrationData.Unit.ounces
+        hd.glassSize = 236.588 //8 oz in mL
+        
+        var expected = 0.0
+        var expected_str = "0.0 oz"
+        #expect(hd.getTotalIntakeFormatted() == expected_str)
+        
+
+        
+        repeat {
+            hd.logGlassOfWater()
+            expected += glassSize_oz
+            if (expected > dailyGoal_oz) {
+                expected = dailyGoal_oz
+            }
+            expected_str = "\(String(format: "%.1f", expected)) oz"
+            #expect(hd.getTotalIntakeFormatted() == expected_str)
+        } while expected < dailyGoal_oz
         
     }
     
