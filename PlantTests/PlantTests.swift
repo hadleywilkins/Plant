3b51e1066e4af262866a5dff104a87a0471b748f
kendall-sullivan <123098060+kendall-sullivan@users.noreply.github.com>
2025-11-12 10:42:46 -0600

    unit testing start

--- PlantTests/PlantTests.swift
+++ PlantTests/PlantTests.swift
@@ -7,11 +7,49 @@
 
 import Testing
 @testable import Plant
+//@testable import HydrationData.Unit
+import SwiftUI
 
-struct PlantTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+class PlantTests {
+    var waterIntake: Double
+    var dailyGoal: Double
+    var unit : HydrationData.Unit
+    var glassSize: Double
+    
+    init() async throws {
+        waterIntake = HydrationData().waterIntake
+        dailyGoal = HydrationData().dailyGoal
+        unit = HydrationData().unit
+        glassSize = HydrationData().glassSize
+        
+    }
+    
+    @Test func testUnitConversion() async throws {
+        let testCase = HydrationData.Unit.liters
+        let result = testCase.value(amountInMilliliters: 100)
+        #expect(result == 0.1)
+        
+        let testCase2 = HydrationData.Unit.ounces
+        let result2 = testCase2.value(amountInMilliliters: 100)
+        #expect(result2 == 3.381405650328842)
     }
 
+    @Test func testOuncesRounding() async throws {
+        
+        let hd = HydrationData()
+//        hd.resetDailyIntake()
+//        let testCase = HydrationData.Unit.ounces
+        HydrationData().waterIntake = 0
+        let result = hd.getTotalIntakeFormatted()
+        #expect(result == "0.0 oz")
+        
+    }
+    
+    deinit {
+        HydrationData().waterIntake = waterIntake
+        HydrationData().dailyGoal = dailyGoal
+        HydrationData().glassSize = glassSize
+        HydrationData().unit = unit
+        
+    }
 }
